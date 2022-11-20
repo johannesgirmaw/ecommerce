d309601433f19f2b259d9b678feb23465a0d128b
yohannes girmaw <yg@etechsc.com>
2022-11-20 23:48:01 +0300

    migrations

--- applications/order/models.py
+++ applications/order/models.py
@@ -17,7 +17,7 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     total_amount = models.DecimalField(
         max_digits=8, decimal_places=2, blank=True, null=True)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2, null=True)
     quantity = models.IntegerField(default=1)
 
     class Meta:

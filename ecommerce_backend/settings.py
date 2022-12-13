7c77bec141927b2ee4f4a593ff0820ff4a498427
yohannes girmaw <yg@etechsc.com>
2022-12-13 09:25:31 +0300

    updates

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -243,4 +243,5 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
+# heroku deployment
 django_heroku.settings(locals())

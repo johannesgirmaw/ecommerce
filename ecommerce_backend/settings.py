5444a22782ac3c45201221bcb8f6e8c6a272b81e
yohannes girmaw <yg@etechsc.com>
2022-12-25 11:18:37 +0300

    token time update

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -211,8 +211,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Managing refresh tokens
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
